7776ed47fe0fb576aa1a721f144f4f15a11a0e4d
LY-QWQ <lyqwq@users.noreply.github.com>
2026-07-24 16:32:53 +0800

    .

--- src/main/java/com/opennight/NightNeo.java
+++ src/main/java/com/opennight/NightNeo.java
@@ -23,17 +23,5 @@ public class NightNeo {
         registerModules();
     }
 
-        private void registerModules() { return;
-        moduleManager.register(new KillAura());
-        moduleManager.register(new Scaffold());
-        moduleManager.register(new ClickGuiModule());
-        moduleManager.register(new NameTags());
-        moduleManager.register(new FullBright());
-        moduleManager.register(new Sprint());
-        moduleManager.register(new SafeWalk());
-        moduleManager.register(new NoFov());
-        moduleManager.register(new NoHurtCam());
-        moduleManager.register(new AntiStaff());
-        moduleManager.register(new FastPlace());
-    }
+        private void registerModules() {} }
 }
